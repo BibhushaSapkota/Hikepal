e81e6d59ba15992e804d569fcee5f382f5761b8d
BibhushaSapkota <Bibhushasapkota5@gmail.com>
2023-06-09 07:28:33 +0545

    commit

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import './About.css';
+
+function About(){
+    return(
+        <div className="about-container">
+            <h1> Our Mission</h1>
+            <p>Our mission is to provide a user-friendly and reliable platform that enables individuals to connect with fellow hikers, discover new trails, and embark on memorable hiking journeys together. We strive to create a safe and inclusive environment where people can build meaningful connections, share their passion for nature, and enjoy the benefits of outdoor activities in the company of compatible companions.</p>
+            <h1>Our Vision</h1>
+            <p>Our vision at Hikepal is to create a global community of outdoor enthusiasts who can easily connect and find like-minded companions for hiking adventures. We aim to foster a sense of exploration, friendship, and shared experiences in the natural beauty of the world, inspiring individuals to embrace an active and fulfilling lifestyle.</p>
+            <h1>Our Goal</h1>
+            <p>Our goal at Hikepal is to connect individuals, prioritize safety, inspire exploration, foster community engagement, and drive continuous improvement to create a seamless and trusted platform for hiking companionship and outdoor adventures worldwide.</p>
+        </div>
+
+    )
+}
+export default About;
